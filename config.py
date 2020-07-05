eada878e0628a3fb1a12dd6e6fcb4f54ad4d9433
Josh Harris <49429928+jharris711@users.noreply.github.com>
2020-07-05 15:02:37 -0400

    db setup/flaskmigrate/sqlalchemy

--- config.py
+++ config.py
@@ -1,5 +1,12 @@
 import os
+from dotenv import load_dotenv
+
+
+base_dir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(base_dir, '.env'))
 
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'secretKey333son'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
