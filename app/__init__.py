cec952948f999f74650be314633b51c6d1a1867b
Josh Harris <49429928+jharris711@users.noreply.github.com>
2020-07-07 12:44:06 -0400

    moment.js date time format

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,11 @@
 import os
 from flask import Flask
 from config import Config
-from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail
+from flask_moment import Moment
 from flask_migrate import Migrate
 from flask_login import LoginManager
-from flask_mail import Mail
+from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
@@ -12,16 +13,25 @@ from logging.handlers import SMTPHandler, RotatingFileHandler
 
 # ---------------------------------------------------------------------------- #
 app = Flask(__name__)
+# Config:
 app.config.from_object(Config)
+# Connect to DB:
 db = SQLAlchemy(app)
+# Set up migrations:
 migrate = Migrate(app, db)
+# Set-up flask-login:
 login = LoginManager(app)
 login.login_view = 'login'
+# Mail:
 mail = Mail(app)
+# Bootstrap:
 bootstrap = Bootstrap(app)
+# Moment.js:
+moment = Moment(app)
 
 
 from app import routes, models, errors
+
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
